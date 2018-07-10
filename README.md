25f67e0b55c2f0a34fa4e79a2efff76d4bb37df8
Abhishek Yadav <zerothabhishek@gmail.com>
2018-07-10 14:17:16 +0530

    Adds sample

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# jobs
-Job posts for Chennai-js
+## Job posts for Chennai-js
+
+Create a file similar to sample.js
+Send a PR, and we'll post it on the group
+
---
+++ sample.js
@@ -0,0 +1,16 @@
+{
+  title:            "Javascript developer",
+  location:         "Chennai",
+  experience:       "3 years",
+  frameworks:       "Angular",
+  freelance_ok:     "Yes",
+  hours_per_week:    10, // in case of freelance
+  company:          "Awesome co",
+  contact_person:   "sample@example.org",
+  job_description:  "https://awesome-co.in/jobs/123", //link or details
+  other_comments:   ""
+}
+
+
+// Please also use https://carbon.now.sh to generate a picture of this post for better sharing
+
